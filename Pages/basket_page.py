b996683ed29c9f13b3d81f144813eafe1bd5f381
Maiia Orlovskaia <orlovsky-maya@mail.ru>
2023-06-19 08:44:04 +0400

    Renamed methods in basket_page

--- Pages/basket_page.py
+++ Pages/basket_page.py
@@ -4,20 +4,20 @@ from Pages.locators import BasketPageLocators
 
 class BasketPage(BasePage):
 
-    def should_be_message_basket_is_empty(self):
+    def should_be_empty_msg(self):
         assert self.is_element_present(BasketPageLocators.MESSAGE_BASKET_IS_EMPTY), "The message 'Your basket is" \
                                                                                     " empty. Continue shopping' " \
                                                                                     "is not presented, but should be"
 
-    def should_not_be_message_basket_is_empty(self):
+    def should_not_be_empty_msg(self):
         assert self.is_not_element_present(BasketPageLocators.MESSAGE_BASKET_IS_EMPTY), "The message 'Your basket is" \
                                                                                         " empty. Continue shopping' " \
                                                                                         "is presented, and should be"
 
-    def should_be_product_in_basket(self):
+    def should_be_product(self):
         assert self.is_element_present(BasketPageLocators.PRODUCT_IS_IN_BASKET), "The product is not in basket, and " \
                                                                                  "should not be"
 
-    def should_not_be_product_in_basket(self):
+    def should_not_be_product(self):
         assert self.is_not_element_present(BasketPageLocators.PRODUCT_IS_IN_BASKET), "The product is in basket, but " \
                                                                                      "should not be"
